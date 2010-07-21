36290a27453babc4887a0534d761195c84467c4a
Pedro Del Gallego <pedro.delgallego@gmail.com>
2010-07-21 17:01:36 +0200

    add if test for form in if true branch

--- test.rkt
+++ test.rkt
@@ -2,10 +2,18 @@
   
 (require rackunit "kernel.rkt")
 
-;; Test Simple data types.
-(check-equal? (eval #f '()) #f "Simple false atom ")
-(check-equal? (eval #t '()) #t "Simple true atom")
 
-(check-equal? (eval 1 '())       1       "Positive number")
-(check-equal? (eval -123 '()) -123       "Negative number")
-(check-equal? (eval "hola" '()) "hola"   "String")
+
+;; Test Simple data types.
+(check-equal? (eval #f (env.initial)) #f "Simple false atom ")
+(check-equal? (eval #t (env.initial)) #t "Simple true atom")
+
+(check-equal? (eval 1 (env.initial))       1       "Positive number")
+(check-equal? (eval -123 (env.initial)) -123       "Negative number")
+
+(check-equal? (eval "hola" (env.initial)) "hola"   "String")
+
+;; The If statement
+(check-equal?  (eval '(if #t "hola" "adios") (env.initial)) "hola"    "false condition in if statament")
+(check-equal?  (eval '(if #f "hola" "adios") (env.initial)) "adios"   "false condition in if statament")
+(check-equal?  (eval '(if #f (+ 1 1) (- 1 1)) (env.initial)) 2        "Execute form in a  if statament true branch")
