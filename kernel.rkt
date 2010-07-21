6deff1cf588d5618fd3c5257050ae94db3150dc4
Pedro Del Gallego <pedro.delgallego@gmail.com>
2010-07-21 21:53:09 +0200

    enviroment accept primitives

--- kernel.rkt
+++ kernel.rkt
@@ -19,26 +19,93 @@
     [(? boolean?) expr]
     [(? string?)  expr]
     [(? number?)  expr]
+    [(? symbol?)  (env.look-up expr env)]
+    [`(set! ,key ,value ) (env.set! env key value)]
 
-    ;; Set a value symbol value. 
-    [`(set! ,key ,value ) (env-set! env key value)]
-    
-    ;; The if expression
     [`(if ,ec ,et ,ef)    (if (eval ec env)
                               (eval et env)
-                              (eval ef env))]))
+                              (eval ef env))]
+    
+    [`(begin ,e1 ,e2)     (begin (eval e1 env)
+                                 (eval e2 env))]))
 
 
 
 ;; -----------------------------------   Enviroment
-(define (env.empty) (hash))
-(define (env.initial) (env.empty))
+(define env.empty (make-weak-hash))
+(define env.initial env.empty)
+(define env.global env.initial)
+ 
 
-(define (env-set! env  key value)
-  (hash-set env key value))
+(define (env.set! env  key value)
+  (hash-set! env key value))
+
+(define (env.look-up expr env )
+  (hash-ref env expr))
 
 
+;; -----------------------------------   Primitives
+(define-syntax definitial 
+  (syntax-rules ()    
+    [(definitial name)
+      (hash-set! env.global name null) ]    
+    [(definitial name value)
+     (hash-set! env.global name value) ] ) )
+
+(define-syntax-rule (defprimitive name value arity)
+  (definitial name 
+    (lambda (values) 
+      (if (= arity (length values))
+          (apply value values)      
+          (error "Incorrect arity"
+                 (list 'name values))))))
+
+(define-syntax-rule (defpredicate  name value arity)
+  (defprimitive name
+    (lambda values (or (apply value values) the-false-value))
+    arity ) )
+
+;; ----------------------------------- 
 
 
-(provide eval env.initial)
-    
+(definitial t #t)
+(definitial f the-false-value)
+(definitial nil '())
+
+(definitial x)
+(definitial y)
+(definitial z)
+(definitial a)
+(definitial b)
+(definitial c)
+(definitial k)
+(definitial foo)
+(definitial bar)
+(definitial hux)
+(definitial fib)
+(definitial fact)
+(definitial visit)
+(definitial length)
+(definitial primes)
+
+(defprimitive cons cons 2)
+(defprimitive car car 1)
+(defprimitive cdr cdr 1)
+(defpredicate pair? pair? 1)
+(defpredicate symbol? symbol? 1)
+(defprimitive eq? eq? 2)           ; cf. exercice \ref{exer-predicate}
+(defpredicate eq? eq? 2)           ; cf. exercice \ref{exer-predicate}
+(defprimitive set-car! set-car! 2)
+(defprimitive set-cdr! set-cdr! 2)
+(defprimitive + + 2)
+(defprimitive - - 2)
+(defpredicate = = 2)
+(defpredicate > > 2)
+(defpredicate < < 2)               ; cf. exercice \ref{exer-predicate}\endlisp
+(defprimitive * * 2)
+(defpredicate <= <= 2)
+(defpredicate >= >= 2)
+(defprimitive remainder remainder 2)
+(defprimitive display display 1)
+
+(provide eval env.global env.initial)
